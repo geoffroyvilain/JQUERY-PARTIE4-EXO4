1af822e2cc4bfe610abc08bf701e774f35a26bec
geoffroyvilain <vilaingeoffroy02@gmail.com>
2019-02-12 11:41:59 +0100

    exo corrigé

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,18 +9,20 @@ $(function(){
 //LES VARIABLES
     var firstname = $('#firstname').val();
     var lastname = $('#lastname').val();
-    var dateOfBirth = $('#dateOfBirth').val();
-    var placeOfBirth = $('#placeOfBirth').val();
+    var birthDate = $('#birthDate').val();
+    var birthPlace = $('#birthPlace').val();
     var work = $('#work').val();
     var nameSociete = $('#nameSociete').val();
 //LES REGEX
-  var regexName = /^([A-Z]{1}[a-zÀ-ÖØ-öø-ÿ]+)([- ]{1}[A-Z]{1}[a-zÀ-ÖØ-öø-ÿ]+){0,1}$/;
-  var regexDateOfBirth = /^[0-2]{1}[0-9]{1}[/]{1}[0-1]{1}[0-9]{1}[/]{1}[0-2]{1}[0-9]{3}$/;
+  var regexName = /^([A-Z]{1}[A-Za-zÀ-ÖØ-öø-ÿ]+)([- ]{1}[A-Z]{1}[a-zÀ-ÖØ-öø-ÿ]+){0,1}$/;
+  var regexBirthDate = /^[0-2]{1}[0-9]{1}[/]{1}[0-1]{1}[0-9]{1}[/]{1}[0-2]{1}[0-9]{3}$/;
+  var regexBirthPlace = /^[A-Z]{1}[A-Za-zéèêàç\-']*/;
+  var regexText = /^[A-Za-z0-9\-, éèê]$/;
 
-  if(firstname.match(regexName) && lastname.match(regexName) && dateOfBirth.match(regexDateOfBirth) /*&& placeOfBirth.match(regex???) && work.match(regex???) && nameSociete.match(regex???)*/){
-    alert('Bonjour, je me présente, je m\'appelle ' + firstname + ' ' + lastname + '.' + ' Je suis né(e) le ' + dateOfBirth + ' à ' + placeOfBirth + '. Je suis actuellement ' + work + ' à ' + nameSociete + '.');
+  if(firstname.match(regexName) && lastname.match(regexName) && birthDate.match(regexBirthDate) /*&& birthPlace.match(regexBirthPlace) && work.match(regexText) && nameSociete.match(regexText)*/){
+    alert('Bonjour, je me présente, je m\'appelle ' + firstname + ' ' + lastname + '.' + ' Je suis né(e) le ' + birthDate + ' à ' + birthPlace + '. Je suis actuellement ' + work + ' à ' + nameSociete + '.');
   }else{
-    alert('Merci de renseigner les champs corrèctement')
+    alert('Merci de renseigner les champs correctement');
   }
 });
 });
